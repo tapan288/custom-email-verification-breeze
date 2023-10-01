3df0e2893fc62db166a6bac117b1ae082fd00a65
tapan288 <tapansharma69@gmail.com>
2023-10-01 16:48:31 +0545

    define custom guard for CustomUser model

--- app/Models/CustomUser.php
+++ app/Models/CustomUser.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class CustomUser extends Model
+class CustomUser extends Authenticatable
 {
     use HasFactory;
+
+    protected $guard = 'customUserAuth';
 
     protected $fillable = [
         'name',
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'customUserAuth' => [
+            'driver' => 'session',
+            'provider' => 'customUsers',
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'customUsers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\CustomUser::class,
         ],
 
         // 'users' => [
